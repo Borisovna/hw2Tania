0dbc43eac8b5b4c9559cd3379894950467b600e9
Tania <0121mymail@gmail.com>
2017-10-27 12:55:43 +0300

    отформатировала код

--- functions.php
+++ functions.php
@@ -1,74 +1,96 @@
 <?php
 
-function task1($var1, $var2 = null)
+function task1 ($var1, $var2 = null)
 {
     if (isset($var2)) {
-        return implode('', $var1);
+        return implode ('', $var1);
     } else {
-        for ($i = 0; $i <= count($var1); $i++) {
+        for ($i = 0; $i <= count ($var1); $i++) {
             echo '<p>' . $var1 [$i] . '</p> </br>';
         }
     }
-
+    
 }
-
 ;
-function task2($nums,$math){
-    if (!is_array ($nums)){
+function task2 ($nums, $math)
+{
+    if (!is_array ($nums)) {
         return '</br>ERROR:первый аргумент не является массивом!';
     }
-    for($i=0; $i<count ($nums);$i++){
-        if(!is_numeric ($nums[$i])){
+    for ($i = 0; $i < count ($nums); $i++) {
+        if (!is_numeric ($nums[$i])) {
             return '</br>ERROR:массив чисел должен содержать только числовые значения!';
         }
     };
     
     $rez = $nums[0];
     
-    for ($i=1; $i<count ($nums);$i++){
+    for ($i = 1; $i < count ($nums); $i++) {
         
-        switch($math){
-            case '+': $rez =$rez+$nums[$i]; break;
-            case '-': $rez =$rez-$nums[$i]; break;
-            case '*': $rez =$rez*$nums[$i]; break;
-            case '/':if ($nums[$i]==0){
-                return '</br>ERROR:на ноль делить нельзя!';
-            }else{
-                $rez =$rez/$nums[$i];
-            }  break;
-            default: return"ERROR:Некорректный ввод арифметического знака<br/>";
+        switch ($math) {
+            case '+':
+                $rez = $rez + $nums[$i];
+                break;
+            case '-':
+                $rez = $rez - $nums[$i];
+                break;
+            case '*':
+                $rez = $rez * $nums[$i];
+                break;
+            case '/':
+                if ($nums[$i] == 0) {
+                    return '</br>ERROR:на ноль делить нельзя!';
+                } else {
+                    $rez = $rez / $nums[$i];
+                }
+                break;
+            default:
+                return "ERROR:Некорректный ввод арифметического знака<br/>";
         }
     }
+    
     return $rez;
-};
-
-function task3()
-{
-    $intervals=func_get_args(); //Возвращает массив, содержащий аргументы функции
-    $operation = $intervals[0];
-    $rez = $intervals[1];
-    for ($i=2; $i<count ($intervals);$i++){
-        switch($operation){
-            case '+': $rez =$rez+$intervals[$i]; break;
-            case '-': $rez =$rez-$intervals[$i]; break;
-            case '*': $rez =$rez*$intervals[$i]; break;
-            case '/':if ($intervals[$i]==0){
-                return '</br>ERROR:на ноль делить нельзя!';
-            }else{
-                $rez =$rez/$intervals[$i];
-            }   break;
-            default: return"ERROR:Некорректный ввод арифметического знака<br/>";
-        }
-    }
-    $arr=array_splice($intervals, 1);//Удаляет часть массива и заменяет её чем-нибудь ещё
-    $str = implode($operation, $arr);// Объединяет элементы массива в строку с разделителем
-    return $str.'='.$rez;
-
 }
 
-function task4($int1, $int2)
+;
+
+function task3 ()
 {
-    if (is_int($int1) and is_int($int2)) {
+    $intervals = func_get_args (); //Возвращает массив, содержащий аргументы функции
+    $operation = $intervals[0];
+    $rez = $intervals[1];
+    for ($i = 2; $i < count ($intervals); $i++) {
+        switch ($operation) {
+            case '+':
+                $rez = $rez + $intervals[$i];
+                break;
+            case '-':
+                $rez = $rez - $intervals[$i];
+                break;
+            case '*':
+                $rez = $rez * $intervals[$i];
+                break;
+            case '/':
+                if ($intervals[$i] == 0) {
+                    return '</br>ERROR:на ноль делить нельзя!';
+                } else {
+                    $rez = $rez / $intervals[$i];
+                }
+                break;
+            default:
+                return 'ERROR:Некорректный ввод арифметического знака<br/>';
+        }
+    }
+    $arr = array_splice ($intervals, 1);//Удаляет часть массива и заменяет её чем-нибудь ещё
+    $str = implode ($operation, $arr);// Объединяет элементы массива в строку с разделителем
+    
+    return $str . '=' . $rez;
+    
+}
+
+function task4 ($int1, $int2)
+{
+    if (is_int ($int1) and is_int ($int2)) {
         echo "<table>";
         for ($i = 1; $i < $int1; $i++) {
             echo "<tr>";
@@ -83,29 +105,30 @@ function task4($int1, $int2)
     }
 }
 
-function task5($str)
+function task5 ($str)
 {
-    $str = mb_strtolower($str); // приведение к нижнему регистру
-    $arr1 = str_split($str);  // разбор строки на массив
+    $str = mb_strtolower ($str); // приведение к нижнему регистру
+    $arr1 = str_split ($str);  // разбор строки на массив
     // переворот массива символов
-    for ($i = 1; $i <= count($arr1); $i++) {
-        $arr2[$i - 1] = $arr1[count($arr1) - $i];
+    for ($i = 1; $i <= count ($arr1); $i++) {
+        $arr2[$i - 1] = $arr1[count ($arr1) - $i];
     }
     // функция проверка на наличие пробелов, новый массив без пробелов
-    function asd($arr)
+    function asd ($arr)
     {
-        for ($i = 0; $i < count($arr); $i++) {
+        for ($i = 0; $i < count ($arr); $i++) {
             if ($arr[$i] !== " ") {
                 $newarr[$i] = $arr[$i];
             };
         };
+        
         return $newarr;
     }
-
-    $newarr1 = asd($arr1);
-    $newarr2 = asd($arr2);
+    
+    $newarr1 = asd ($arr1);
+    $newarr2 = asd ($arr2);
 //сравниваем дассивы на равность
-    for ($i = 0; $i < count($newarr1); $i++) {
+    for ($i = 0; $i < count ($newarr1); $i++) {
         if ($newarr1[$i] !== $newarr2[$i]) {
             return false;
         } else {
@@ -115,7 +138,7 @@ function task5($str)
 }
 
 ;
-function output($a)
+function output ($a)
 {
     if ($a == true) {
         echo "заданое значение полиндром";
@@ -124,15 +147,10 @@ function output($a)
     }
 }
 
-//function task8($str){
-//    $pattern ='/[0-9]/';
-//    $arr=preg_grep ($pattern,explode (":",$str);
-//    return $arr;
-//}
-
-function task9($file){
-    $hand = fopen($file, "r");
-    $contents = fread($hand, filesize($file));
-    fclose($hand);
+function task9 ($file)
+{
+    $hand = fopen ($file, "r");
+    $contents = fread ($hand, filesize ($file));
+    fclose ($hand);
     return $contents;
 }
--- index.php
+++ index.php
@@ -1,56 +1,52 @@
 <?php
-require('functions.php');
-$a=[' строка1 ','строка2 ','строка3 ','строка4'];
-$b='1';
-echo 'TASK1'.'</br>';;
-$rez=task1($a,$a);
-echo $rez.'</br>';
+require ('functions.php');
 
-echo '</br>'.'TASK2'.'</br>';
-$number=array(5,1,10,1);
-$q='h';
-$znak='/';
-echo task2($number,$znak);
+$a = [' строка1 ', 'строка2 ', 'строка3 ', 'строка4'];
+$b = '1';
+echo 'TASK1</br>';
+$rez = task1 ($a, $a);
+echo $rez . '</br>';
+
+echo '</br>TASK2</br>';
+$number = [5, 1, 10, 1];
+$q = 'h';
+$znak = '/';
+echo task2 ($number, $znak);
 echo '</br>';
 
-echo '</br>'.'TASK3'.'</br>';
-echo  task3('+',10,0,3,6,8,1).'</br>';
+echo '</br>TASK3</br>';
+echo task3 ('+', 10, 0, 3, 6, 8, 1) . '</br>';
 
-echo '</br>'.'TASK4'.'</br>';
-echo  task4(5,7).'</br>';
+echo '</br>TASK4</br>';
+echo task4 (5, 7) . '</br>';
 
-echo 'TASK5'.'</br>';
-$b=task5('5s5');
-echo output($b).'</br>'.'</br>';
+echo 'TASK5</br>';
+$b = task5 ('5s5');
+echo output ($b) . '</br>' . '</br>';
 
-echo 'TASK6'.'</br>';
-echo date("m.d.Y G:i");
-echo '</br>'.time();
+echo 'TASK6</br>';
+echo date ("m.d.Y G:i");
+echo '</br>' . time ();
 
-echo '</br>'.'</br>'.'TASK7'.'</br>';
-$str="Карл у Клары украл Кораллы";
-echo preg_replace('![A-Z|А-Я]!u','', $str).'</br>';
-$str2="Две бутылки лимонада";
-$arr2=explode (' ',$str2);
-for ( $i=0; $i<count ($arr2);$i++){
-    if ($arr2[$i]=="Две"){
-        $arr2[$i]="Три";
+echo '</br></br>TASK7</br>';
+$str = 'Карл у Клары украл Кораллы';
+echo preg_replace ('![A-Z|А-Я]!u', '', $str) . '</br>';
+$str2 = 'Две бутылки лимонада';
+$arr2 = explode (' ', $str2);
+for ($i = 0; $i < count ($arr2); $i++) {
+    if ($arr2[$i] == 'Две') {
+        $arr2[$i] = 'Три';
     }
 }
-$srt3=implode (' ',$arr2);
+$srt3 = implode (' ', $arr2);
 echo $srt3;
 
-echo '</br>'.'</br>'.'TASK9'.'</br>';
-$file="test.txt";
-echo task9($file);
+echo '</br></br>TASK9</br>';
+$file = 'test.txt';
+echo task9 ($file);
 
-//fopen ($file,"w");
-//$current = "Hello, world";
-//file_put_contents($file, $current);
-//echo file_get_contents($file);
-
-echo '</br>'.'</br>'.'TASK10'.'</br>';
-$file="anothertest.txt";
-fopen ($file,"w");
-$current = "Hello again!";
-file_put_contents($file, $current);
+echo '</br></br>TASK10</br>';
+$file = 'anothertest.txt';
+fopen ($file, "w");
+$current = 'Hello again!';
+file_put_contents ($file, $current);
